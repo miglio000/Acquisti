c0a644e3cac8b71f02cd56a1b3f88d95c7a33c01
miglio000 <gabrielemiglio23@gmail.com>
2020-12-22 14:39:30 +0100

    Add project files.

---
+++ Compratore/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Negozio;
+namespace Compratore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                string nome = GestioneProdotti.Nome();
+                double prezzo = GestioneProdotti.Prezzo(i);
+                int quantità = GestioneProdotti.Quantità(i);
+                double costo_totale = GestioneProdotti.CostoTotale(prezzo, quantità);
+                Console.WriteLine($"Il totale del {i + 1}° prodotto chiamato '{nome}' è {costo_totale} euro");
+            }
+        }
+    }
+}
---
+++ Negozio/GestioneProdotti.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Negozio
+{
+    public class GestioneProdotti
+    {
+        public static string Nome()
+        {
+            Console.Write("Inserisci nome prodotto: ");
+            string nome = Console.ReadLine();
+            return nome;
+        }
+        public static double Prezzo(int c)
+        {
+            double prezzo;
+            do
+            {
+                Console.WriteLine($"Inserisci costo {c + 1}° prodotto: ");
+                prezzo = Convert.ToDouble(Console.ReadLine());
+            } while (prezzo <= 0);
+            return prezzo;
+        }
+        public static int Quantità(int c)
+        {
+            int quantità;
+            Console.WriteLine($"Inserisci quante volte vuoi acquistare il {c + 1}° prodotto: ");
+            quantità = Convert.ToInt32(Console.ReadLine());
+            return quantità;
+        }
+        public static double CostoTotale(double prezzo, int quantità)
+        {
+            double costo_totale = prezzo * quantità;
+            return costo_totale;
+        }
+    }
+}
